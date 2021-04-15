cfcbe51780df456c4540720d27ca83db758659f0
Tobin Bergen-Hill <TBERGENHILL@MITRE.ORG>
2021-04-15 17:04:14 -0400

    Added distribution bundle script, server bat file and XML file

---
+++ bundle.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+echo Setting up VASSAL distribution directory "/c/temp/VASSAL"
+rm -rf /c/temp/VASSAL
+mkdir /c/temp/VASSAL
+cp -r release-prepare/target/lib /c/temp/VASSAL
+cp dist/windows/VASSAL.* /c/temp/VASSAL
+cp vassal-app/src/vassal.xml /c/temp/VASSAL
+cp -r release-prepare/target/doc /c/temp/VASSAL
+cp VASSAL_server.bat /c/temp/VASSAL
+echo Changing directories
+pushd /c/temp/ 
+mv VASSAL/lib/vassal-app-3.6.0-SNAPSHOT.jar VASSAL/lib/Vengine.jar
+echo Zipping up directory for distribution
+zip -r -q VASSAL.zip VASSAL
+echo Moving back to original directory
+popd
+echo Done
+
+
